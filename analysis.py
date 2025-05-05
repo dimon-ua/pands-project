08a2e045dd0eadff609cc4ab56e5e20d60835ddc
unknown <dima.kozlovskyy@gmail.com>
2025-05-05 21:38:47 +0100

    creating the first variable from the data set

--- analysis.py
+++ analysis.py
@@ -7,7 +7,26 @@ with open('iris/iris.data', 'r') as f:
     data = f.read()  # the output is a string type.
     
 
-# Finding how many variables does this dataset has.
+arr_string = data.split()  # we split the string into a list of strings
+
+# we create an empty list for the first column
+column_1 = []  
+
+# it will iteration in next step
+index = 0
+
+
+# looping through the whole arr_string list, extracting the first item from each row
+for each_item in arr_string:
+    for each_item_index_0 in arr_string[index]:
+        column_1.append(each_item_index_0)
+        index += 1
+        
+        
+print(column_1)  # we print the first column
+
+
+
 
 
 
