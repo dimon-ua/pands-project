895e1f3f418eb1618cde07124e38ddab6ce2db57
unknown <dima.kozlovskyy@gmail.com>
2025-05-05 22:51:48 +0100

    creating a func for the same task

--- analysis.py
+++ analysis.py
@@ -13,23 +13,29 @@ arr_string = data.split('\n')
 
 # creating an empy list where we will add frst item from each row
 column_1 = []  
+column_2 = []
+column_3 = []
+column_4 = []
 
-
-# looping through the whole arr_string list, extracting the first item from each row
-for each_row in arr_string:
-    # in dataset file last two rows are empty, so we need to skip them and add only items with numbers
-    if each_row == '':
-        continue
-    # we split each row by comma and add the first item to the column_1 list
-    column_1.append(each_row.split(',')[0])      
+column_index = 0  # iterate throwugh the items in a list
 
     
-print(column_1[-1])
-# print(column_1)  # we print the first column
 
+# As we need to create for columns, its better to create a function that will do the same job for each column
+def column_create(column, index):
+    # looping through the whole arr_string list, extracting the first item from each row
+    for each_row in arr_string:
+        # in dataset file last two rows are empty, so we need to skip them and add only items with numbers
+        if each_row == '':
+            continue
+        # we split each row by comma and add the first item to the column_1 list
+        column.append(each_row.split(',')[index])  
 
+column_create(column_1, 0)  # first column
+column_create(column_2, 1)  # second column
+column_create(column_3, 2)  # third column
+column_create(column_4, 3)  # fourth column
 
-
-
+print(column_4)  # print the first column
 
 # Now we need to get an access to the first variable. 
