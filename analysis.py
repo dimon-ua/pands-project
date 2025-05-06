2d48c3fa60ae55b946ca45ead9fc0b20af2f6087
unknown <dima.kozlovskyy@gmail.com>
2025-05-06 13:41:25 +0100

    column_create func works properly

--- analysis.py
+++ analysis.py
@@ -1,26 +1,26 @@
 # as the project is divided into several files, we need to import the functions from other files
-from data_loader import data_load
+from data_loader import data_load as dl
 from column_extractor import column_create
 
 
 # this means that this file will be run as a script, not inported
 if __name__ == "__main__":
-    data = data_load()  # calling the function to load the data from the file
-    print(data)  # print the data to check if it is loaded correctly
-    # creating an empy list where we will add frst item from each row
-    # https://en.wikipedia.org/wiki/Iris_flower_data_set
-    # Creating a correct order of variables from the columns
-    sepal_length_var = []  
-    sepal_width_var = []
-    petal_length_var = []
-    petal_width_var = []
-    
-    # using the function to create each column
-    sepal_length_var = column_create(data, 0)  # first column with 1st element
-    column_create(sepal_width_var, 1)  # second column with 2nd element
-    column_create(petal_length_var, 2)  # third column with 3rd element
-    column_create(petal_width_var, 3)  # fourth column with 4th element
+    column = dl()  # load the dataset from the file
+    # print(column)  # print the whole dataset
 
-    # print(sepal_length_var)  # print the first column
+    # https://en.wikipedia.org/wiki/Iris_flower_data_set
+    # Creating a correct order of variables from the columns 
+    # [sepal_length_var, 
+    # sepal_width_var, 
+    # petal_length_var, 
+    # petal_width_var]
+    
+    
+    # extracting from column_create func a correct column
+    sepal_length_var = column_create(column, 0)  # first column with 1st element
+    sepal_width_var = column_create(column, 1)  # second column with 2nd element
+    petal_length_var = column_create(column, 2)  # third column with 3rd element
+    petal_width_var = column_create(column, 3)  # fourth column with 4th element
+     
 
 
--- column_extractor.py
+++ column_extractor.py
@@ -1,14 +1,17 @@
+
 # As we need to create four columns, its better to create a function that will do the same job for each column
-def column_create(rows, index):    
-    # looping through the whole arr_string list, extracting the first item from each row
-    column =[]
+def column_create(rows, index):  
+    column = []
     for each_row in rows:
+        
         # in dataset file last two rows are empty, so we need to skip them and add only items with numbers
-        if each_row.strip() == '':
+        if each_row == '':
             continue
+        
         # we split each row by comma and add the first item to the column_1 list
-        rows.append(each_row.split(',')[index])      
-    return column 
-    
+        column.append(each_row.split(',')[index])    
+    return column
+     
+     
     
     
