265f0ffc7e754f923f25895430b44b2584ff47b4
unknown <dima.kozlovskyy@gmail.com>
2025-05-07 12:45:11 +0100

    os.path.join added to save png's in a folder

--- hist_variables.py
+++ hist_variables.py
@@ -2,6 +2,10 @@
 # https://matplotlib.org/2.0.2/users/pyplot_tutorial.html
 import matplotlib.pyplot as plt
 
+# Importing os to be able to store pics in a folder
+# https://docs.python.org/3/library/os.html#module-os
+import os
+
 def hist_plot(column, name_variable):
             # Plotting the data, histograms on each variables
             # https://matplotlib.org/stable/gallery/statistics/hist.html
@@ -13,4 +17,14 @@ def hist_plot(column, name_variable):
         plt.title(name_variable + ' Histogram')
         
         # Show the plot
-        plt.show()
+        # plt.show()
+        
+        if not os.path.exists('histograms'):
+            os.makedirs('histograms')  # Create the folder if it doesn't exist
+        
+        # Saving the plot to a folder
+        # https://matplotlib.org/stable/api/_as_gen/matplotlib.figure.Figure.savefig.html#matplotlib-figure-figure-savefig
+        # Using os.path.join to create the path to the folder and save the png files
+        # https://docs.python.org/3/library/os.path.html
+        plt.savefig(os.path.join('histograms', f'{name_variable}.png'), format='png')
+        
